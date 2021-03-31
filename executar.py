adcba99e73b1d47e93709587be7bcbc3d21a6e6a
GigasMesh <40374830+GigasMesh@users.noreply.github.com>
2021-03-30 21:02:28 -0300

    Factor: Implementação da Discord Slash

--- executar.py
+++ executar.py
@@ -1,12 +1,15 @@
 from discord.ext import commands, tasks
 from funcionalidades import Lembrete
 from discord.embeds import Embed
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 import string
 import servicos
 from funcionalidades.funcionalidade_lembretes.funcoes_auxiliares import retorna_hora
 
 cliente = commands.Bot(command_prefix='?')
 conexao = servicos.repositorio.pega_conexao()
+slash = SlashCommand(cliente, sync_commands=True)
 lembrete = Lembrete(conexao)
 lembrete.atualizar_lista_de_bancos()
 
@@ -15,15 +18,38 @@ lembrete.atualizar_lista_de_bancos()
 async def on_ready():
     print('Kaburagi Iniciado!')
 
-
-@cliente.command()
-async def lembretes(contexto):
+@slash.slash(name="klembretes", 
+             guild_ids=[460678660559470592], 
+             description="Lista todos os lembretes do servidor.")
+async def _lembretes(contexto):
     banco_existe, resultado = lembrete.mostra_lembretes(contexto)
     await contexto.send(embed=resultado)
 
 
-@cliente.command()
-async def adicionar_lembrete(contexto, *args):
+@slash.slash(name="kadicionar_lembrete",
+             guild_ids=[460678660559470592],
+             description="Adiciona um lembrete.",
+             options=[
+                 create_option(
+                 name="nome",
+                 description="Nome do lembrete.",
+                 option_type=3,
+                 required=True
+               ),
+                 create_option(
+                 name="dia",
+                 description="Dia no qual você quer ser lembrado.",
+                 option_type=3,
+                 required=True
+               ),
+                 create_option(
+                 name="adicional",
+                 description="Alguma informação adicional para o lembrete.",
+                 option_type=3,
+                 required=False
+               ),
+             ])
+async def _adicionar_lembrete(contexto, *args):
     flag, index_do_dia = lembrete.interpreta_mensagem(args)
     if flag:
         dia = string.capwords(args[index_do_dia])
@@ -36,54 +62,58 @@ async def adicionar_lembrete(contexto, *args):
                                               'Segunda, Terça, Quarta, Quinta, Sexta, Sábado ou Domingo.'))
 
 
-@cliente.command()
-async def al(contexto, *args):
-    flag, index_do_dia = lembrete.interpreta_mensagem(args)
-    if flag:
-        dia = string.capwords(args[index_do_dia])
-        nome = string.capwords(' '.join(args[0:index_do_dia]))
-        adicional = ' '.join(args[index_do_dia + 1:])
-        resultado = lembrete.adicionar_lembrete(contexto, nome, dia, adicional)
-        await contexto.send(embed=resultado)
-    else:
-        await contexto.send(embed=Embed(title='Dia não encontrado na  posição correta, escreva no formato:\n' +
-                                              'Segunda, Terça, Quarta, Quinta, Sexta, Sábado ou Domingo.'))
 
-
-@cliente.command()
-async def remover_lembrete(contexto, *args):
-    nome = string.capwords(' '.join(args))
+@slash.slash(name="kremover_lembrete", 
+             guild_ids=[460678660559470592],
+             description="Remove um lembrete do servidor.",
+             options=[
+                 create_option(
+                 name="nome",
+                 description="Nome do lembrete a ser removido.",
+                 option_type=3,
+                 required=True
+               )
+             ])
+async def _remover_lembrete(contexto, nome):
     removido, mensagem = lembrete.remover_lembrete(contexto, nome)
     await contexto.send(embed=Embed(title='%s' % mensagem))
 
 
-@cliente.command()
-async def rl(contexto, *args):
-    nome = string.capwords(' '.join(args))
-    removido, mensagem = lembrete.remover_lembrete(contexto, nome)
-    await contexto.send(embed=Embed(title='%s' % mensagem))
-
-
-@cliente.command()
-async def ajuda(contexto):
+@slash.slash(name="kajuda", 
+             guild_ids=[460678660559470592],
+             description="Exibe todos os comandos e suas descrições."
+             )
+async def _ajuda(contexto):
     await contexto.send(embed=lembrete.ajuda())
 
 
-@cliente.command()
-async def hoje(contexto):
+@slash.slash(name="khoje", 
+             guild_ids=[460678660559470592],
+             description="Exibe os lembretes correspondentes ao dia atual."
+             )
+async def _hoje(contexto):
     banco_existe, resultado = lembrete.hoje(contexto.guild.id)
     await contexto.send('%s' % contexto.author.mention)
     await contexto.send(embed=resultado)
 
 
-@cliente.command()
-async def editar_informacao_adicional(contexto, *args):
+@slash.slash(name="keditar_informacao_adicional", 
+             guild_ids=[460678660559470592],
+             description="Edita as informações adicionais de um lembrete.",
+             options=[
+                 create_option(
+                 name="nome",
+                 description="Nome do lembrete a ser editado.",
+                 option_type=3,
+                 required=True
+               ),
+             ])
+async def _editar_informacao_adicional(contexto, nome):
     def check(mensagem):
         return contexto.author == mensagem.author and mensagem.channel == mensagem.channel
 
 
-    nome = string.capwords(' '.join(args))
-    if not args or not lembrete.verifica_se_nome_existe(contexto, nome):
+    if not lembrete.verifica_se_nome_existe(contexto, nome):
         await contexto.send(embed=Embed(title="Informe um lembrete válido"))
         return 0
     await contexto.send(embed=Embed(title="O que devo colocar nas informações adicionais de %s?" % nome))
@@ -91,18 +121,23 @@ async def editar_informacao_adicional(contexto, *args):
     await contexto.send(embed=lembrete.editar_informacao_adicional(contexto, nome, mensagem.content))
 
 
-@cliente.command()
-async def eia(contexto, *args):
-    await editar_informacao_adicional(contexto, *args)
-
-@cliente.command()
-async def ed(contexto, *args):
+@slash.slash(name="keditar_dia", 
+             guild_ids=[460678660559470592],
+             description="Edita o dia de um lembrete.",
+             options=[
+                 create_option(
+                 name="nome",
+                 description="Nome do lembrete a ser editado.",
+                 option_type=3,
+                 required=True
+               ),
+             ])
+async def _editar_dia(contexto, nome):
 
     def check(mensagem):
         return contexto.author == mensagem.author and mensagem.channel == mensagem.channel
 
-    nome = string.capwords(' '.join(args))
-    if not args or not lembrete.verifica_se_nome_existe(contexto, nome):
+    if not lembrete.verifica_se_nome_existe(contexto, nome):
         await contexto.send(embed=Embed(title="Informe um lembrete válido"))
         return 0
     await contexto.send(embed=Embed(title="Para qual dia devo mudar %s?" % nome))
@@ -111,22 +146,11 @@ async def ed(contexto, *args):
     await contexto.send(embed=lembrete.editar_dia(contexto, nome, mensagem))
 
 
-@cliente.command()
-async def editar_dia(contexto, *args):
-    await ed(contexto, *args)
-
-
-@cliente.command()
-async def mensagens_diarias(contexto):
-    mensagem = Embed(title="Para receber mensagens diárias dos lembretes no servidor, faça:")
-    mensagem.add_field(name="Crie um canal para o Kaburagi", value="O nome do canal deve ser: kaburagi", inline=False)
-    mensagem.add_field(name="Crie um cargo para o Kaburagi marcar", value="O nome do cargo deve ser: Esquecido",
-                       inline=False)
-    await contexto.send(embed=mensagem)
-
-
-@cliente.command()
-async def md(contexto):
+@slash.slash(name="kmensagens_diarias", 
+             guild_ids=[460678660559470592],
+             description="Instruções para receber as mensagens diárias do Kaburagi."
+             )
+async def _mensagens_diarias(contexto):
     mensagem = Embed(title="Para receber mensagens diárias dos lembretes no servidor, faça:")
     mensagem.add_field(name="Crie um canal para o Kaburagi", value="O nome do canal deve ser: kaburagi", inline=False)
     mensagem.add_field(name="Crie um cargo para o Kaburagi marcar", value="O nome do cargo deve ser: Esquecido",
@@ -136,7 +160,7 @@ async def md(contexto):
 
 @tasks.loop(minutes=1)
 async def called_once_a_day():
-    horarios_para_avisar = ['12:00', '20:00', '22:30']
+    horarios_para_avisar = ['09:00', '17:00', '19:30']
     for servidor in cliente.guilds:
         if retorna_hora() in horarios_para_avisar:
             message_channel = None
