9d738aebb30ff2e3f3efb3aaf8fe407b42a17efe
Iuryglabson <iuryglabson18@gmail.com>
2021-07-07 12:01:54 -0300

    Fix: Parâmetro autor deve ser opcional

--- servicos/manipulacao_de_embed.py
+++ servicos/manipulacao_de_embed.py
@@ -1,7 +1,7 @@
 from servicos.informacoes_sobre_tempo import retorna_data_hora
 
 
-def adiciona_info(embed, autor):
+def adiciona_info(embed, autor=None):
     embed.set_footer(text='Kaburagi', icon_url='https://i.ibb.co/1LB5zCv/5x38c88c1eb51.png')
     embed.timestamp = retorna_data_hora()
     if autor:
